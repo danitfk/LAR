f2c2be7e51747d79caf4312708158b2cd18203f4
Daniel Gordi <9387078+danitfk@users.noreply.github.com>
2020-08-04 17:58:16 +0200

    Update dependency_installer.sh

--- dependency_installer.sh
+++ dependency_installer.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+check_kali=$(grep -o kali /etc/debian_version)
+if [[ "$check_kali" == "kali" ]]; then
+echo "OS detected successfully"
+else
+echo "OS Not supported. Only KALI LINUX"
+exit 1
 echo "Step 1/6: Running apt-get update"
 apt-get update
 echo ""
 echo "Installing packages"
 echo "Step 2/6: Checking/Installing Theharvester"
-apt install theharvester
+apt install theharvester -qy
 echo ""
 echo "Step 3/6: Checking/Installing Whois"
-apt install whois
+apt install whois -qy
 echo ""
 echo "Step 4/6: Checking/Installing DNSrecom"
-apt install dnsrecon
+apt install dnsrecon -qy
 echo ""
 echo "Step 5/6: Checking/Installing Metagoofil"
-apt install metagoofil
+apt install metagoofil -qy
 echo ""
 echo "Step 6/6: Checking/Installing Knockpy"
-apt install knockpy
+apt install knockpy -qy
 echo ""
 echo "Done"
